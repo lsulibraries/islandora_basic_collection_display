d1146a98d40141dc36e7ce27aa891251edc336c3
Will <wwconlin@gmail.com>
2018-09-12 15:17:42 -0500

    template restored. unneeded functions deleted

--- theme/islandora-basic-collection-grid.tpl.php
+++ theme/islandora-basic-collection-grid.tpl.php
@@ -9,8 +9,11 @@
 
 <div class="islandora islandora-basic-collections">
   <div class="islandora-basic-collection-grid clearfix">
-    <?php foreach ($variables['rendered_grids'] as $grid) : ?>
-      <?php print render($grid); ?>
+    <?php foreach ($variables['associated_objects_array'] as $object) : ?>
+      <dl class='islandora-basic-collection-object <?php print $object['class']; ?>'>
+        <dt class='islandora-basic-collection-thumb'><?php print $object['thumb_link']; ?> </dt>
+        <dd class='islandora-basic-collection-caption'> <?php print filter_xss($object['title_link']); ?> </dd>
+      </dl>
     <?php endforeach; ?>
 </div>
 </div>
