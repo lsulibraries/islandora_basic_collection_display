8e18895407662c2c76384760b84a33a24ebc8ef7
Will <wwconlin@gmail.com>
2018-09-13 09:50:10 -0500

    replace theme from solution pack with one from our theme folder

--- theme/islandora-basic-collection-wrapper.tpl.php
+++ theme/islandora-basic-collection-wrapper.tpl.php
@@ -9,13 +9,16 @@
 ?>
 
 <div class="islandora-basic-collection-wrapper">
+<!--    <h1 class="lp-title"><?php //print $landing_page_title  ?></h1>-->
+    <h1 class="about-link"><?php print $about_link  ?></h1>
   <?php if (!$display_metadata && !empty($dc_array['dc:description']['value'])): ?>
-    <p><?php print nl2br($dc_array['dc:description']['value']); ?></p>
+    <div class="collection-description-text"><?php print nl2br($dc_array['dc:description']['value']); ?></div>
     <hr />
   <?php endif; ?>
   <div class="islandora-basic-collection clearfix">
     <span class="islandora-basic-collection-display-switch">
       <ul class="links inline">
+          <li><span class="islandora-basic-collection-item-count"><?php print $total_count ?> items</span></li>
         <?php foreach ($view_links as $link): ?>
           <li>
             <a <?php print drupal_attributes($link['attributes']) ?>><?php print filter_xss($link['title']) ?></a>
@@ -32,7 +35,7 @@
       <?php print $description; ?>
       <?php if ($parent_collections): ?>
         <div>
-          <h2><?php print t('In collections'); ?></h2>
+          <h2><?php print t('In collectionz'); ?></h2>
           <ul>
             <?php foreach ($parent_collections as $collection): ?>
               <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
