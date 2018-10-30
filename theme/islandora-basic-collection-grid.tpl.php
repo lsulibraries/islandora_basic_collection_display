9f23f4fbd8a8d4f5cfe47057f6b6026b776e938d
ktanglao <ktanglao@lsu.edu>
2018-10-30 10:22:32 -0500

    Reorders caption

--- theme/islandora-basic-collection-grid.tpl.php
+++ theme/islandora-basic-collection-grid.tpl.php
@@ -9,10 +9,18 @@
 
 <div class="islandora islandora-basic-collections helloKyle">
   <div class="islandora-basic-collection-grid clearfix">
+    <div class='collectionHeader masonryItem image_header'>
+        <div class="itemTitle" id="page-title">[Page Title]</div>   
+        <div class="headerBreadcrumb">[breadcrumb]</div>        
+        <div class="userMenu">
+            <div class="infoToggle userSelect"><div class="iconSelect"></div><div class="textSelect">details</div></div>
+            <div id="shareToggle" class="userSelect"><div class="iconSelect"></div><div class="textSelect">share</div></div>            
+        </div>        
+    </div>
     <?php foreach ($variables['associated_objects_array'] as $object) : ?>
-      <div class='islandora-basic-collection-object <?php print $object['class']; ?>'>
+      <div class='islandora-basic-collection-object masonryItem <?php print $object['class']; ?>'>
+        <div class='islandora-basic-collection-caption'> <?php print filter_xss($object['title_link']); ?> </div>      
         <div class='islandora-basic-collection-thumb'><?php print $object['thumb_link']; ?> </div>
-        <div class='islandora-basic-collection-caption'> <?php print filter_xss($object['title_link']); ?> </div>
       </div>
     <?php endforeach; ?>
 </div>
